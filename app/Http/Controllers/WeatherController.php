94c89a01ea68ba971c1d914d28f1a806924646ac
abd <alssadiabdalrahman5@gmail.com>
2026-05-04 23:48:29 +0300

    addweather

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Responses\Response;
+use App\Services\WeatherService;
+use Illuminate\Http\Request;
+use Throwable;
+
+class WeatherController extends Controller
+{
+    protected WeatherService $weatherService;
+
+    public function __construct(WeatherService $weatherService)
+    {
+        $this->weatherService = $weatherService;
+    }
+
+    public function getTodayByProject($projectId)
+    {
+        try {
+            $data = $this->weatherService->getTodayWeatherForProject($projectId);
+
+            return Response::success(
+                $data['message'],
+                $data['data'],
+                (int) $data['status']
+            );
+        } catch (Throwable $throwable) {
+            $code = is_int($throwable->getCode()) && $throwable->getCode() > 0
+                ? $throwable->getCode()
+                : 500;
+
+            return Response::error($throwable->getMessage(), $code);
+        }}
+}
