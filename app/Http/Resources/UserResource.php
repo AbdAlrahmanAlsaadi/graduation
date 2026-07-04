a24371156bbf805fa79cbfd136a384ae9c39f575
Wiaam409 <wiaam409@gmail.com>
2026-07-04 19:57:46 +0300

    Owner

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'          => $this->id,
+            'name'        => $this->name,
+            'email'       => $this->email,
+            'internal_id' => $this->internal_id,
+            'status'      => $this->status,
+            'roles'       => $this->whenLoaded('roles', fn () => $this->getRoleNames()),
+            'created_at'  => $this->created_at?->toISOString(),
+            'updated_at'  => $this->updated_at?->toISOString(),
+        ];
+    }
+}
