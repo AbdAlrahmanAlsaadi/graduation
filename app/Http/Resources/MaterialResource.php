079d7a15547a0108a8428b178b5c97b7441cc0a1
abd <alssadiabdalrahman5@gmail.com>
2026-08-22 14:15:10 +0300

    messi

--- app/Http/Resources/MaterialResource.php
+++ app/Http/Resources/MaterialResource.php
@@ -7,18 +7,20 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class MaterialResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
             'unit' => $this->unit,
-            'work_item_name' => $this->whenLoaded('workItems')->pluck('name')->unique()->values(),
+
+            'work_item_name' => $this->when(
+                $this->relationLoaded('workItems'),
+                fn() => $this->workItems
+                    ->pluck('name')
+                    ->unique()
+                    ->values()
+            ),
         ];
     }
-    }
+}
